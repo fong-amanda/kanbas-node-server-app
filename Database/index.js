cf7ba542e3fc41800e2d7b0975109b71a6161057
fong-amanda <fong.m.amanda@gmail.com>
2023-11-18 19:00:42 -0500

    new changes

--- Database/index.js
+++ Database/index.js
@@ -1,9 +1,9 @@
-import courses from "./courses";
-import modules from "./modules";
-import assignments from "./assignments";
-import users from "./users";
-import grades from "./grades";
-import enrollments from "./enrollments";
+import courses from "./courses.json" assert { type: "json" };
+import modules from "./modules.json" assert { type: "json" };
+import assignments from "./assignments.json" assert { type: "json" };
+import users from "./users.json" assert { type: "json" };
+import grades from "./grades.json" assert { type: "json" };
+import enrollments from "./enrollments.json" assert { type: "json" };
 export default {
   courses,
   modules,
